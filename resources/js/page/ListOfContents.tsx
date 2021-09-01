734d3325238398a1f330c3f240f9d7c0c680c2ce
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-09-01 15:17:21 +0700

    Refactor code

--- resources/js/page/ListOfContents.tsx
+++ resources/js/page/ListOfContents.tsx
@@ -1,11 +1,10 @@
 import APIv2PostService from "../service/APIv2PostService";
 import ListOfContentsJSXElement from "../components/post/ListOfContentsJSXElement";
-import APIv2FileService from "../service/APIv2FileService";
+import { lazy } from "react";
 
 const ListOfContents = () => {
     return ListOfContentsJSXElement({
         postService: new APIv2PostService(),
-        fileService: new APIv2FileService(),
     });
 };
 
